2d633768c94b0ac34b09cb5f8b1f18503c6495d4
EllieHield <42339621+EllieHield@users.noreply.github.com>
2019-05-16 11:34:10 +0100

    refactored emailservice to omit powershell

--- server/src/main/kotlin/com/scottlogic/reactivegame/services/EmailService.kt
+++ server/src/main/kotlin/com/scottlogic/reactivegame/services/EmailService.kt
@@ -2,59 +2,24 @@ package com.scottlogic.reactivegame.services
 
 import com.microsoft.outlook.ClassFactory
 import com.microsoft.outlook.OlItemType
+import com.microsoft.outlook._Application
 import com.microsoft.outlook._MailItem
+import com4j.Com4jObject
 import org.springframework.stereotype.Service
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.nio.file.Paths
-
 
 @Service
 class EmailService {
-    fun sendEmail(emailAddress: String): Int {
-        emailAddress
-        val subject = "Worm World request access link"
-        val body = "hello Ellie"
-        //getting the file path of sendEmail.ps1
-        val path = Paths.get(javaClass.classLoader.getResource("sendEmail.ps1").toURI()).toAbsolutePath().toString()
-        //constructing the command
-        val powerShellProcess = ProcessBuilder().command("powershell.exe", path, "-to", "'$emailAddress'", "-subject", "'$subject'", "-body", "'$body'").start()
-        // Getting the results
-        val stdout = BufferedReader(InputStreamReader(
-                powerShellProcess.inputStream))
-        stdout.useLines { it.forEach(::println) }
-        //getting any errors
-        val stderr = BufferedReader(InputStreamReader(
-                powerShellProcess.errorStream))
-        stderr.useLines {
-            val lines = it.joinToString("\n")
-            if (lines.isNotBlank()) {
-                throw EmailException(lines, powerShellProcess.exitValue())
-            }
+    fun sendEmail(address: String, body: String) {
+        val application: _Application = ClassFactory.createApplication()
+        val comObject: Com4jObject = application.createItem(OlItemType.olMailItem)
+        try {
+            val mailItem: _MailItem = comObject.queryInterface(_MailItem::class.java)
+            mailItem.to(address)
+            mailItem.subject("Worm World request access link")
+            mailItem.body(body)
+            mailItem.send()
+        } finally {
+            comObject.dispose()
         }
-        return powerShellProcess.exitValue()
-    }
-
-    fun sendEmail2() {
-//        param (
-//                [Parameter(Mandatory=$true,Position=0)][string]$to,
-//        [Parameter(Mandatory=$true,Position=1)][string]$subject,
-//        [Parameter(Mandatory=$true,Position=2)][string]$body
-//        )
-//        $Outlook = New-Object -ComObject Outlook.Application
-//        $Mail = $Outlook.CreateItem(0)
-//        $Mail.To = "$to"
-//        $Mail.Subject = "$subject"
-//        $Mail.Body = "$body"
-//        $Mail.Send()
-//        [System.Runtime.Interopservices.Marshal]::ReleaseComObject($Outlook) | Out-Null
-
-        val application = ClassFactory.createApplication()
-        val comObject = application.createItem(OlItemType.olMailItem)
-        val mailItem: _MailItem = comObject.queryInterface(_MailItem::class.java)
-//        mailItem.
-//        mailItem.queryInterface()
     }
 }
-
-class EmailException(message:String, val exitCode: Int): Exception(message)
--- server/src/test/kotlin/com/scottlogic/reactivegame/EmailServiceTests.kt
+++ server/src/test/kotlin/com/scottlogic/reactivegame/EmailServiceTests.kt
@@ -1,19 +1,15 @@
 package com.scottlogic.reactivegame
 
-import com.scottlogic.reactivegame.services.EmailException
 import com.scottlogic.reactivegame.services.EmailService
 import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.CoreMatchers.not
-import org.hamcrest.Matchers.hasProperty
 import org.junit.Assert.assertThat
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.ExpectedException
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
-import org.junit.rules.ExpectedException
-
 
 
 @RunWith(SpringRunner::class)
@@ -25,24 +21,24 @@ class EmailServiceTests {
 
     @get:Rule
     val exception: ExpectedException = ExpectedException.none()
-
-    @Test
-    fun sendEmailTestHappy() {
-        val exitCode = emailService.sendEmail("ehield@scottlogic.com' 'hacked subject' 'whatever I want'; Write-Output \"hello\"; Write-Output '")
-        assertThat(exitCode, equalTo(0))
-    }
-
-    @Test
-    fun sendEmailTestSad() {
-        exception.expect(EmailException::class.java)
-        exception.expect(hasProperty<EmailException>("exitCode", not(equalTo(0))))
-        val exitCode = emailService.sendEmail("ehield@scottlogic.com")
-        assertThat(exitCode, equalTo(1))
-    }
+//
+//    @Test
+//    fun sendEmailTestHappy() {
+//        val exitCode = emailService.sendEmail("ehield@scottlogic.com' 'hacked subject' 'whatever I want'; Write-Output \"hello\"; Write-Output '")
+//        assertThat(exitCode, equalTo(0))
+//    }
+//
+//    @Test
+//    fun sendEmailTestSad() {
+//        exception.expect(EmailException::class.java)
+//        exception.expect(hasProperty<EmailException>("exitCode", not(equalTo(0))))
+//        val exitCode = emailService.sendEmail("ehield@scottlogic.com")
+//        assertThat(exitCode, equalTo(1))
+//    }
 
     @Test
     fun sendEmail2TestHappy() {
-        val nada = emailService.sendEmail2()
+        val nada = emailService.sendEmail(address = "ehield@scottlogic.com", body = "hello Ellie, \nI'm passing in a variable")
         assertThat(nada, equalTo(Unit))
     }
 
